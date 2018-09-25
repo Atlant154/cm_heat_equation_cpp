c1d79d5152ff98c73c851eb6301b4da05e737378
vlis <vlis.next@gmail.com>
2018-09-25 22:52:10 +0700

    added tridiagonal_matrix class

--- include/tridiagonal_matrix.h
+++ include/tridiagonal_matrix.h
@@ -1,61 +1,33 @@
 #ifndef PROJECT_TRIDIAGONAL_MATRIX_H
 #define PROJECT_TRIDIAGONAL_MATRIX_H
 
-#define X_LEFT_BOUND 0
-#define X_RIGHT_BOUND 1
-#define TIME_LEFT_BOUND 0
-#define TIME_RIGHT_BOUND 1
+#define X_LEFT_BOUND 0.0
+#define X_RIGHT_BOUND 1.0
+#define TIME_LEFT_BOUND 0.0
+#define TIME_RIGHT_BOUND 1.0
 
 #include<vector>
 class tridiagonal_matrix {
 public:	
-	tridiagonal_matrix() {}
-	~tridiagonal_matrix() {}
+	tridiagonal_matrix() {};
+	~tridiagonal_matrix() {};
 	
-	tridiagonal_matrix(const long long h_num, const long long time_layers_num) {
-		set_h(h_num);
-		set_tau(time_layers_num);
-		n = h_num;
-		set_above();
-		set_main();
-		set_below()
-	}
+	tridiagonal_matrix(const unsigned int h_num, const unsigned int time_layers_num);
 	
-	long long get_h() {
-		return h;
-	};
+	long double get_h();
 private:
-	void set_h(const long long h_num) {
-		h = (X_RIGHT_BOUND - X_LEFT_BOUND) / h_num;
-	}
+	void set_h(const unsigned int h_num);
 	
-	void set_tau(const long long time_layers_num) {
-		tau = (TIME_RIGHT_BOUND - TIME_LEFT_BOUND) / time_layers_num;
-	}
+	void set_tau(const unsigned int time_layers_num);
 	
-		void set_above() {
-		above.reserve(n-1);
-		for(int i=0; i < n-1; ++i) {
-			above[i] = i;
-		}
-	}
+	void set_above();
 	
-	void set_main() {
-		main.reserve(n)
-		for(int i=0; i < n; ++i) {
-			main[i] = 2*i*i;
-		}		
-	}
+	void set_main();
 	
-	void set_below() {
-		below.reserve(n-1)
-		for(int i=0; i < n-1; ++i) {
-			below[i] = i;
-		}		
-	}
+	void set_below();
 	
 	long double h = 0;
-	long long n = 0;
+	unsigned int n = 0;
 	long double tau = 0;
 	std::vector<long double> above = {0};
 	std::vector<long double> main = {0};
--- src/tridiagonal_matrix.cpp
+++ src/tridiagonal_matrix.cpp
@@ -1,1 +1,43 @@
 #include "../include/tridiagonal_matrix.h"
+
+tridiagonal_matrix::tridiagonal_matrix(const unsigned int h_num, const unsigned int time_layers_num) {
+	set_h(h_num);
+	set_tau(time_layers_num);
+	n = h_num;
+	set_above();
+	set_main();
+	set_below();
+};
+
+long double tridiagonal_matrix::get_h() {
+	return h;
+};
+
+void tridiagonal_matrix::set_h(const unsigned int h_num) {
+	h = (X_RIGHT_BOUND - X_LEFT_BOUND) / h_num;
+};
+
+void tridiagonal_matrix::set_tau(const unsigned int time_layers_num) {
+	tau = (TIME_RIGHT_BOUND - TIME_LEFT_BOUND) / time_layers_num;
+};
+
+void tridiagonal_matrix::set_above() {
+	above.reserve(n-1);
+	for(int i=0; i < n-1; ++i) {
+		above[i] = i;
+	}
+};
+
+void tridiagonal_matrix::set_main() {
+	main.reserve(n);
+		for(int i=0; i < n; ++i) {
+		main[i] = 2*i*i;
+	}		
+};
+	
+void tridiagonal_matrix::set_below() {
+	below.reserve(n-1);
+	for(int i=0; i < n-1; ++i) {
+		below[i] = i;
+	}		
+};
