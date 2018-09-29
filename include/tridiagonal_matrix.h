1522e024aece79614942cca96516637e3502c70e
Пацакула Никита <nicman2222@gmail.com>
2018-09-29 21:40:21 +0700

    WIP: appearance improved, TDMA fixed(partly), constructor improved.

--- include/tridiagonal_matrix.h
+++ include/tridiagonal_matrix.h
@@ -16,39 +16,23 @@ public:
 
 	std::vector<long double> get_result_();
 private:
-	void set_h();
+	double function_of_heat_sources(long double x, long double t);
 
-	void set_tau();
+	double function_of_exact_solution(long double x, long double t);
 
-	long double f(long double x, long double t);
-
-	long double u(long double x, long double t);
-
-	long double mu_0(long double x);
-
-	long double mu_1(long double t);
-
-	long double mu_2(long double t);
-
-	void set_above();
-
-	void set_main();
-
-	void set_below();
-
-	void set_u();
-
+	//Time and space boundaries + diffusivity coefficient(a):
 	const double x_left_bound = 0.0;
 	const double x_right_bound = 1.0;
 	const double time_left_bound = 0.0;
-	const double time_right_bound = 1.1;
+	const double time_right_bound = 1.0;
+	const double a = 0.0021;
 
-	const double h = 0;
-	const unsigned int n = 0;
-	const double tau = 0;
-	const double a = 0.0022;
-	const unsigned int h_num = 0;
-	const unsigned int time_layers_num = 0;
+	//Unknown at compile time(define if constructor):
+	const unsigned int h_num;
+	const unsigned int time_layers_num;
+
+	double h;
+	double tau;
 	std::vector<long double> above = {0};
 	std::vector<long double> main = {0};
 	std::vector<long double> below = {0};
--- src/tridiagonal_matrix.cpp
+++ src/tridiagonal_matrix.cpp
@@ -1,13 +1,13 @@
 #include "../include/tridiagonal_matrix.h"
 
-tridiagonal_matrix::tridiagonal_matrix(unsigned int h_num, unsigned int time_layers_num) : h_num(h_num), time_layers_num(time_layers_num)
+tridiagonal_matrix::tridiagonal_matrix(unsigned int h_num, unsigned int time_layers_num) : h_num(h_num),
+																						   time_layers_num(time_layers_num)
 {
-	set_h();
-	set_tau();
-	n = h_num;
-	set_above();
-	set_main();
-	set_below();
+	this -> h = (x_right_bound - x_left_bound) / h_num;
+	this -> tau = (time_right_bound - time_left_bound) / time_layers_num;
+    above.reserve(h_num - 1);
+	main.reserve(h_num);
+	below.reserve(h_num - 1);
 }
 
 long double tridiagonal_matrix::get_h_() {
@@ -18,79 +18,36 @@ long double tridiagonal_matrix::get_tau_() {
     return tau;
 }
 
-void tridiagonal_matrix::set_h() {
-	h = (x_right_bound - x_right_bound) / h_num;
-}
-
-void tridiagonal_matrix::set_tau() {
-	tau = (time_right_bound - time_left_bound) / time_layers_num;
-}
-
-void tridiagonal_matrix::set_above() {
-	above.reserve(n-1);
-}
-
-void tridiagonal_matrix::set_main() {
-	main.reserve(n);
-}
-
-void tridiagonal_matrix::set_below() {
-	below.reserve(n-1);
-}
-
-long double tridiagonal_matrix::f(long double x, long double t) {
+double tridiagonal_matrix::function_of_heat_sources(long double x, long double t)
+{
 	return 1;
 }
 
-long double tridiagonal_matrix::u(long double x, long double t) {
+double tridiagonal_matrix::function_of_exact_solution(long double x, long double t)
+{
 	return 1;
-}
-
-long double tridiagonal_matrix::mu_0(long double x) {
-	return 1;
-}
-
-long double tridiagonal_matrix::mu_1(long double t) {
-	return 1;
-}
-
-long double tridiagonal_matrix::mu_2(long double t) {
-	return 1;
-}
-
-void tridiagonal_matrix::set_u() {
-	/*TODO correct the logic*/
-	u.reserve((h_num + 1) * time_layers_num)
-	for(unsigned int j = 0; j < h_num + 1; ++j) {
-		u[j] = mu_1(tau*j)
-		u[n*h_num + j] = mu_2[tau*j]
-	}
-
-	for(unsigned int i = 0; i < time_layers_num; ++i) {
-		u[h_num*i] = mu_0(h*) // WIP
-	}
 }
 
 std::vector<long double> tridiagonal_matrix::get_result_()
 {
-	std::vector<long double> w;
-	w.reserve(n-1);
-	std::vector<long double> g;
-	g.reserve(n);
-	std::vector<long double> p;
-	p.reserve(n);
-	w[0] = below[0]/main[0];
-	g[0] = free[0]/main[0];
-
-	for(unsigned int i = 1; i < n-1; ++i) {
-		w[i] = below[i]/(main[i] - above[i-1]*w[i-1]);
-		g[i] = (free[i] - above[i-1]*g[i-1])/(main[i] - above[i-1]*w[i-1]);
+	std::vector<long double> alpha;
+	alpha.reserve(h_num-1);
+	std::vector<long double> beta;
+	beta.reserve(h_num);
+	std::vector<long double> local_result;
+	local_result.reserve(h_num);
+	alpha[0] = below[0] / main[0];
+	beta[0] = free[0] / main[0];
+	//Get method coefficient:
+	for(auto i = 1; i < h_num - 1; ++i) {
+		alpha[i] = below[i] / (main[i] - above[i - 1] * alpha[i - 1]);
+		beta[i] = (free[i] - above[i - 1] * beta[i - 1]) / (main[i] - above[i - 1] * alpha[i - 1]);
 	}
-	g[n-1] = (free[n-1] - above[n-2]*g[n-2])/(main[n-1] - above[n-2]*w[n-2]);
-
-	p[n-1] = g[n-1];
-	for(unsigned int j = n-1; j > 0; --j) {
-		p[j-1] = g[j-1] - w[j-1]*p[j];
+	beta[h_num - 1] = (free[h_num - 1] - above[h_num - 2] * beta[h_num - 2]) / (main[h_num - 1] - above[h_num - 2] * alpha[h_num - 2]);
+	//Find result on time layer:
+	local_result[h_num - 1] = beta[h_num - 1];
+	for(auto j = h_num - 1; j > 0; --j) {
+		local_result[j - 1] = beta[j - 1] - alpha[j - 1] * local_result[j];
 	}
-	return p;
+	return local_result;
 }
