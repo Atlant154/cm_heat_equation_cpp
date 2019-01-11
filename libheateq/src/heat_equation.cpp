1bfaad113668abded940e784c2f8efc3e9f04e2c
Пацакула Никита <nicman2222@gmail.com>
2019-01-12 02:59:14 +0700

    Design improved: std::size_t -> uint32_t, header including improved.

--- libheateq/src/heat_equation.cpp
+++ libheateq/src/heat_equation.cpp
@@ -1,4 +1,4 @@
-#include "../include/heat_equation.h"
+#include <heat_equation.h>
 
 heat_equation::heat_equation(double (*heat_sources)(double, double),
                              double (*exact_solution)(double, double),
@@ -73,16 +73,16 @@ heat_equation::heat_equation(double (*heat_sources)(double, double),
     results_.clear();
     results_.reserve(time_layers_num_);
 
-    for (std::size_t iter = 0; iter < h_num_; ++iter)
+    for (uint32_t iter = 0; iter < h_num_; ++iter)
         time_layer.emplace_back(initial_time_layer(iter * h_));
 
     results_.emplace_back(time_layer);
 
     double const courant_number = (a_ * tau_) / pow(h_, 2);
 
-    for (std::size_t time_iter = 1; time_iter < time_layers_num_; ++time_iter)
+    for (uint32_t time_iter = 1; time_iter < time_layers_num_; ++time_iter)
     {
-        for (std::size_t space_iter = 1; space_iter < h_num_ - 1; ++space_iter)
+        for (uint32_t space_iter = 1; space_iter < h_num_ - 1; ++space_iter)
             ta_free_part.emplace_back(
                 time_layer[space_iter]
                     + tau_ * heat_sources(x_left_bound_ + space_iter * h_, time_left_bound_ + time_iter * tau_));
@@ -95,7 +95,7 @@ heat_equation::heat_equation(double (*heat_sources)(double, double),
 
         time_layer.clear();
         time_layer.emplace_back(left_bound(time_left_bound_ + time_iter * tau_));
-        std::copy(ta_result.begin(), ta_result.end(), std::back_inserter(time_layer));
+        time_layer.insert(time_layer.end(), ta_result.begin(), ta_result.end());
         time_layer.emplace_back(right_bound(time_left_bound_ + time_iter * tau_));
 
         results_.emplace_back(time_layer);
@@ -112,7 +112,7 @@ void heat_equation::modified_thomas_alg(std::vector<double> const & free_part, s
     alpha[n - 2] = -matrix_above_ / matrix_main_;
     beta[n - 2] = free_part.back() / matrix_main_;
 
-    for (size_t iter = n - 3; iter > 0; iter--)
+    for (auto iter = n - 3; iter > 0; iter--)
     {
         common_factor = 1.0 / (matrix_main_ + matrix_above_ * alpha[iter + 1]);
         alpha[iter] = -matrix_above_ * common_factor;
@@ -128,13 +128,13 @@ void heat_equation::modified_thomas_alg(std::vector<double> const & free_part, s
 void heat_equation::write_result(std::string const & path) const
 {
     std::fstream result_file;
-    result_file.open(path + "/tau:" + std::to_string(tau_) + "_h:" + std::to_string(h_) + "_result.txt",
+    result_file.open(path + "/result.txt",
                      std::ios::out | std::ios::trunc);
     result_file << "[[" << x_left_bound_ << "," << x_right_bound_ << ", " << h_num_ << "],"
                 << "[" << time_left_bound_ << "," << time_right_bound_ << ", " << time_layers_num_ << "],[";
-    for (std::size_t time_iter = 0; time_iter < time_layers_num_; ++time_iter) {
+    for (uint32_t time_iter = 0; time_iter < time_layers_num_; ++time_iter) {
         result_file << "[";
-        for (std::size_t space_iter = 0; space_iter < h_num_; ++space_iter) {
+        for (uint32_t space_iter = 0; space_iter < h_num_; ++space_iter) {
             result_file << results_[time_iter][space_iter];
             if (space_iter + 1 != h_num_)
                 result_file << ",";
@@ -154,10 +154,10 @@ void heat_equation::write_error_plot(double (*exact_solution)(double, double)) c
     result_file.open("../result/error.txt", std::ios::out | std::ios::trunc);
     result_file << "[[" << x_left_bound_ << "," << x_right_bound_ << ", " << h_num_ << "],"
                 << "[" << time_left_bound_ << "," << time_right_bound_ << ", " << time_layers_num_ << "],[";
-    for (std::size_t time_iter = 0; time_iter < time_layers_num_; ++time_iter)
+    for (uint32_t time_iter = 0; time_iter < time_layers_num_; ++time_iter)
     {
         result_file << "[";
-        for (std::size_t space_iter = 0; space_iter < h_num_; ++space_iter)
+        for (uint32_t space_iter = 0; space_iter < h_num_; ++space_iter)
         {
             error = std::abs(exact_solution(space_iter * h_, time_iter * tau_) - results_[time_iter][space_iter]);
             result_file << error;
@@ -177,8 +177,8 @@ double heat_equation::get_error(double (*exact_solution)(double, double)) const
     double error = 0.0;
     double abs_appr, abs_exact;
 
-    for (std::size_t time_iter = 1; time_iter < time_layers_num_; ++time_iter)
-        for (std::size_t space_iter = 1; space_iter < h_num_ - 1; ++space_iter)
+    for (uint32_t time_iter = 1; time_iter < time_layers_num_; ++time_iter)
+        for (uint32_t space_iter = 1; space_iter < h_num_ - 1; ++space_iter)
         {
             abs_appr = std::abs(results_[time_iter][space_iter]);
             abs_exact = std::abs(exact_solution(x_left_bound_ + space_iter * h_, time_left_bound_ + time_iter * tau_));
--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include <heat_equation.h>
 
 double const diffusivity_coefficient = 0.020;
-std::string g_path = ".";
+
+std::string g_outputPath = "../result";
 bool g_write = false;
 bool g_help = false;
-uint32_t g_tau_num = 0;
-uint32_t g_h_num = 0;
+uint32_t g_tauNum = 0;
+uint32_t g_hNum = 0;
 
 double function_of_heat_sources(double x, double t) {
     return 2 * t - exp(x) + x - diffusivity_coefficient * ((-1) * t * exp(x) - 12 * pow(x, 2));
@@ -35,20 +36,22 @@ bool parse_arguments(int argc, char ** argv) {
              {"splits",      required_argument, nullptr, 's'},
              {"output-path", required_argument, nullptr, 'o'},
              {"write",       no_argument,       nullptr, 'w'},
-             {"help",        no_argument,       nullptr, 'h'}
+             {"help",        no_argument,       nullptr, 'h'},
+             {nullptr,       0,                 nullptr, 0  }
             };
 
     int32_t opt;
-    while ((opt = getopt(argc, argv, "t:s:o:h:w")) != -1) {
+    int32_t opt_index;
+    while ((opt = getopt_long(argc, argv, "t:s:o:h:w", long_options, &opt_index)) != -1) {
         switch (opt) {
             case 't': {
                 int64_t const tau_candidate = std::stoi(optarg);
-                g_tau_num = tau_candidate > 0 ? (uint32_t) tau_candidate : 0;
+                g_tauNum = tau_candidate > 0 ? (uint32_t) tau_candidate : 0;
             }
                 break;
             case 's': {
                 int64_t const h_candidate = std::stoi(optarg);
-                g_h_num = h_candidate > 0 ? (uint32_t) h_candidate : 0;
+                g_hNum = h_candidate > 0 ? (uint32_t) h_candidate : 0;
             }
                 break;
             case 'w':
@@ -61,7 +64,7 @@ bool parse_arguments(int argc, char ** argv) {
                 g_help = true;
         }
     }
-    return g_h_num != 0 && g_tau_num != 0 && !g_help;
+    return g_hNum != 0 && g_tauNum != 0 && !g_help;
 }
 
 int main(int argc, char ** argv) {
@@ -71,14 +74,14 @@ int main(int argc, char ** argv) {
     }
 
     heat_equation
-            test_class_member(function_of_heat_sources, function_of_exact_solution, diffusivity_coefficient, g_h_num,
-                              g_tau_num);
+            test_class_member(function_of_heat_sources, function_of_exact_solution, diffusivity_coefficient, g_hNum,
+                              g_tauNum);
 
     double error = test_class_member.get_error(function_of_exact_solution);
     std::cout << "Error resulting from the calculation: " << error << "." << std::endl;
 
     if (g_write) {
-        test_class_member.write_result(g_path);
+        test_class_member.write_result(g_outputPath);
         test_class_member.write_error_plot(function_of_exact_solution);
     }
 
